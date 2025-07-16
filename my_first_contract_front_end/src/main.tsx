ace48363ca708e3510b820f1cf2d1153233f3119
Evgeniy Kuznetsov <e.kuznetsov@agora.ru>
2025-07-16 13:40:59 +0300

    move manifest file

--- my_first_contract_front_end/src/main.tsx
+++ my_first_contract_front_end/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import {TonConnectUIProvider} from "@tonconnect/ui-react"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const manifestUrl = "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json"
+
+createRoot(document.getElementById("root")!).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </StrictMode>,
-)
+  </TonConnectUIProvider>,
+);
