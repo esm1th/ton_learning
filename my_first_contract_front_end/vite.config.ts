e7f1140f5fa75743327e8f2db43faf7bb1982eed
Evgeniy Kuznetsov <e.kuznetsov@agora.ru>
2025-08-05 02:04:26 +0300

    fixes

--- my_first_contract_front_end/vite.config.ts
+++ my_first_contract_front_end/vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: "/ton_learning/my_first_contract_frontend/",
+  base: "/my_first_contract_front_end/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
